83ccf28046a8748a8ba61eb61f8a3cb923cf6ca8
adib-commits <hdadib442@gmail.com>
2025-10-21 07:17:01 +0000

    puts

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,2 +1,11 @@
-C
-puts("\"Programming is like building a multilingual puzzle"); return (0);
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+    puts("\"Programming is like building a multilingual puzzle");
+    return (0);
+}
