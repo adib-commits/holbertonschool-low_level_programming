580ca883b419117d5479180582b9336d5bc8adf2
adib-commits <hdadib442@gmail.com>
2025-10-21 13:41:56 +0000

    number

--- variables_if_else_while/6-print_numberz.c
+++ variables_if_else_while/6-print_numberz.c
@@ -6,12 +6,12 @@
  */
 int main(void)
 {
-    int i;
+int i;
 
-    for (i = 0; i < 10; i++)
-        putchar(i + 48);  /* 48 is ASCII code for '0' */
+for (i = 0; i < 10; i++)
+putchar(i + 48);  /* 48 is ASCII code for '0' */
 
-    putchar(10);  /* 10 is ASCII code for newline '\n' */
+putchar(10);  /* 10 is ASCII code for newline '\n' */
 
-    return (0);
+return (0);
 }
