1264256b023bb2aba431950d097631492b28b619
adib-commits <hdadib442@gmail.com>
2025-10-23 12:25:25 +0000

    print alpha

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints 10 times the alphabet in lowercase
+ * followed by a new line
+ */
+void print_alphabet_x10(void)
+{
+    int i;
+    char ch;
+    char alphabet[27] = "abcdefghijklmnopqrstuvwxyz\n";
+
+    for (i = 0; i < 10; i++)
+    {
+        for (ch = 0; alphabet[ch] != '\0'; ch++)
+            _putchar(alphabet[ch]);
+    }
+}
