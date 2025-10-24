5667784d36167fa2e28fb8ba039d6c27b88c7cb6
adib-commits <hdadib442@gmail.com>
2025-10-24 12:56:00 +0000

    print

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,13 +1,20 @@
-#include <unistd.h>
+#include "main.h"
 
 /**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
+ * main - Entry point
+ * 
+ * Return: Always 0 (Success)
  */
-int _putchar(char c)
+
+int main(void)
 {
-    return (write(1, &c, 1));
+char phrase[] = "_putchar\n";
+int i;
+
+for (i = 0; phrase[i] != '\0'; i++)
+{
+_putchar(phrase[i]);
+}
+
+return (0);
 }
