568c4792b4dfe5adc93e74c8c2102fba6b096435
adib-commits <hdadib442@gmail.com>
2025-10-24 14:04:18 +0000

    fg

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -2,13 +2,13 @@
 
 void print_alphabet_x10(void)
 {
-int i;
+int ligne;
 char ch;
 char alphabet[27] = "abcdefghijklmnopqrstuvwxyz\n";
 
-for (i = 0; i < 10; i++)
+for (ligne = 0; ligne < 10; ligne++)
 {
-for (ch = 0; alphabet[ch] != '\0'; ch++)
-_putchar(alphabet[ch]);
+	for (ch = 0; alphabet[ch] != '\0'; ch++)
+		_putchar(alphabet[ch]);
 }
 }
