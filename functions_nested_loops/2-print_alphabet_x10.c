5d220dfbc06e0babca521f313abf132b935414b9
adib-commits <hdadib442@gmail.com>
2025-10-28 15:39:37 +0000

    print alpha

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -1,14 +1,20 @@
 #include "main.h"
 
+/**
+ * print_alphabet_x10 - prints 10 times the alphabet, in lowercase,
+ * followed by a new line
+ */
 void print_alphabet_x10(void)
 {
-int ligne;
-char ch;
-char alphabet[27] = "abcdefghijklmnopqrstuvwxyz\n";
+int i;
+char c;
 
-for (ligne = 0; ligne < 10; ligne++)
+for (i = 0; i < 10; i++)
 {
-	for (ch = 0; alphabet[ch] != '\0'; ch++)
-		_putchar(alphabet[ch]);
+for (c = 'a'; c <= 'z'; c++)
+{
+_putchar(c);
+}
+_putchar('\n');
 }
 }
