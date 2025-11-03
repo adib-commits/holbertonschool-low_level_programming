5569ea619e12cd543746f82b93891fe4abf8d6ad
adib-commits <hdadib442@gmail.com>
2025-11-03 08:55:50 +0000

    1-memcpy.c

--- pointers_arrays_strings/1-memcpy.c
+++ pointers_arrays_strings/1-memcpy.c
@@ -1,30 +1,21 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
- * simple_print_buffer - prints buffer in hexa
- * @buffer: the address of memory to print
- * @size: the size of the memory to print
+ * _memcpy - copies memory area
+ * @dest: destination memory area
+ * @src: source memory area
+ * @n: number of bytes to copy
  *
- * Return: Nothing.
+ * Return: pointer to dest
  */
-void simple_print_buffer(char *buffer, unsigned int size)
+char *_memcpy(char *dest, char *src, unsigned int n)
 {
 unsigned int i;
 
-i = 0;
-while (i < size)
+for (i = 0; i < n; i++)
 {
-if (i % 10)
-{
-printf(" ");
+dest[i] = src[i];
 }
-if (!(i % 10) && i)
-{
-printf("\n");
-}
-printf("0x%02x", buffer[i]);
-i++;
-}
-printf("\n");
+
+return (dest);
 }
