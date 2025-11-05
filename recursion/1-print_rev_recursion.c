491690cd16821def2b2ad2e9bb801bf1b45628c4
adib-commits <hdadib442@gmail.com>
2025-11-05 09:41:22 +0000

    1-print_rev_recursion.c

--- recursion/1-print_rev_recursion.c
+++ recursion/1-print_rev_recursion.c
@@ -2,9 +2,7 @@
 
 /**
  * _print_rev_recursion - prints a string in reverse
- * @s: the string to be printed
- *
- * Return: nothing
+ * @s: pointer to the string to print
  */
 void _print_rev_recursion(char *s)
 {
