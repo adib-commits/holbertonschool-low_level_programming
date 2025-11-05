45ccffe496b1cf45f15159b0224314f077525b2e
adib-commits <hdadib442@gmail.com>
2025-11-05 09:26:32 +0000

    0-puts_recursion.c

--- recursion/0-puts_recursion.c
+++ recursion/0-puts_recursion.c
@@ -1,18 +1,18 @@
 #include "main.h"
 
 /**
- * _puts_recursion - prints a string, followed by a new line
- * @s: pointer to the string to print
+ * _puts_recursion - prints a string followed by a new line
+ * @s: string to print
  *
  * Return: void
  */
 void _puts_recursion(char *s)
 {
-if (*s == '\0')  /* Base case: end of string */
+if (*s == '\0')
 {
 _putchar('\n');
 return;
 }
-_putchar(*s);            /* Print current character */
-_puts_recursion(s + 1);  /* Recurse to print the next character */
+_putchar(*s);
+_puts_recursion(s + 1);
 }
