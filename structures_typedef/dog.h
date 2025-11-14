724e2d37ac077e11e3cfd1e317a8884d400c2783
adib-commits <hdadib442@gmail.com>
2025-11-14 10:28:48 +0000

    dog.h

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -2,21 +2,19 @@
 #define DOG_H
 
 /**
- * struct dog - struct that stores some information about a dog
+ * struct dog - information about dogs
  * @name: Dog's name
  * @age: Dog's age
  * @owner: Dog's owner
- *
- * Description: Longer description if needed.
  */
 struct dog
 {
-char *name;
+char *name
 float age;
 char *owner;
 };
 
-void init_dog(struct dog *d, char *name, float age, char *owner);
-void print_dog(struct dog *d);
+
+typedef struct dog dog_t;
 
 #endif
