098311ec8d077e16367a881ba3287b20c4cbbad5
adib-commits <hdadib442@gmail.com>
2025-11-14 14:33:47 +0000

    4-new_dog.c

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -4,28 +4,30 @@
 /**
  * _strlen - returns the length of a string
  * @s: string
+ *
  * Return: length of string
  */
 int _strlen(char *s)
 {
 int i = 0;
 
-while (s[i] != '\0')
+while (s[i])
 i++;
 return (i);
 }
 
 /**
  * _strcpy - copies a string
- * @dest: destination buffer
- * @src: source string
- * Return: dest
+ * @dest: destination
+ * @src: source
+ *
+ * Return: pointer to dest
  */
 char *_strcpy(char *dest, char *src)
 {
 int i;
 
-for (i = 0; src[i] != '\0'; i++)
+for (i = 0; src[i]; i++)
 dest[i] = src[i];
 dest[i] = '\0';
 
@@ -36,40 +38,43 @@ return (dest);
  * new_dog - creates a new dog
  * @name: dog's name
  * @age: dog's age
- * @owner: owner's name
+ * @owner: dog's owner
  *
- * Return: pointer to new dog_t, or NULL on failure
+ * Return: pointer to new dog_t, or NULL if it fails
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-dog_t *dog;
+dog_t *d;
 int name_len, owner_len;
 
-dog = malloc(sizeof(dog_t));
-if (dog == NULL)
+if (name == NULL || owner == NULL)
+return (NULL);
+
+d = malloc(sizeof(dog_t));
+if (d == NULL)
 return (NULL);
 
 name_len = _strlen(name);
 owner_len = _strlen(owner);
 
-dog->name = malloc(name_len + 1);
-if (dog->name == NULL)
+d->name = malloc(sizeof(char) * (name_len + 1));
+if (d->name == NULL)
 {
-free(dog);
+free(d);
 return (NULL);
 }
 
-dog->owner = malloc(owner_len + 1);
-if (dog->owner == NULL)
+d->owner = malloc(sizeof(char) * (owner_len + 1));
+if (d->owner == NULL)
 {
-free(dog->name);
-free(dog);
+free(d->name);
+free(d);
 return (NULL);
 }
 
-_strcpy(dog->name, name);
-dog->age = age;
-_strcpy(dog->owner, owner);
+_strcpy(d->name, name);
+_strcpy(d->owner, owner);
+d->age = age;
 
-return (dog);
+return (d);
 }
