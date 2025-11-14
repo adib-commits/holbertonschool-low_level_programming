bc71d784f14d934fab461f875d6ac59f24de472c
adib-commits <hdadib442@gmail.com>
2025-11-14 08:56:52 +0000

    1-string_nconcat.c

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -1,41 +1,50 @@
 #include "main.h"
 #include <stdlib.h>
-#include <string.h>
 
 /**
  * string_nconcat - concatenates s1 and the first n bytes of s2
  * @s1: first string
  * @s2: second string
- * @n: maximum number of bytes from s2 to concatenate
+ * @n: maximum number of bytes of s2 to concatenate
  *
- * Return: pointer to newly allocated concatenated string, or NULL on failure
+ * Return: pointer to newly allocated memory with s1 + n bytes of s2,
+ * or NULL on failure
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-char *result;
-unsigned int len1 = 0, len2 = 0, i, total_len;
+unsigned int len1 = 0, len2 = 0, i, j;
+char *ptr;
 
-if (s1)
-len1 = strlen(s1);
-if (s2)
-len2 = strlen(s2);
+if (s1 == NULL)
+s1 = "";
+if (s2 == NULL)
+s2 = "";
+
+
+while (s1[len1] != '\0')
+len1++;
+
+while (s2[len2] != '\0')
+len2++;
+
 
 if (n > len2)
 n = len2;
 
-total_len = len1 + n;
 
-result = malloc(sizeof(char) * (total_len + 1));
-if (!result)
+ptr = malloc(len1 + n + 1);
+if (ptr == NULL)
 return (NULL);
 
+
 for (i = 0; i < len1; i++)
-result[i] = s1[i];
+ptr[i] = s1[i];
 
-for (i = 0; i < n; i++)
-result[len1 + i] = s2[i];
 
-result[total_len] = '\0';
+for (j = 0; j < n; j++)
+ptr[len1 + j] = s2[j];
 
-return (result);
+ptr[len1 + n] = '\0';
+
+return (ptr);
 }
