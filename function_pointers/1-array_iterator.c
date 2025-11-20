03fb9c98811e21949c798a80d11ebb5c1319652e
adib-commits <hdadib442@gmail.com>
2025-11-20 09:06:04 +0000

    1-array_iterator.c

--- function_pointers/1-array_iterator.c
+++ function_pointers/1-array_iterator.c
@@ -1,8 +1,9 @@
-#include "function_pointers.h"
 #include <stddef.h>
+#include "function_pointers.h"
 
 /**
- * array_iterator - executes a function on each element of an array
+ * array_iterator - executes a function given as a parameter
+ *                  on each element of an array
  * @array: array of integers
  * @size: size of the array
  * @action: pointer to the function to use
