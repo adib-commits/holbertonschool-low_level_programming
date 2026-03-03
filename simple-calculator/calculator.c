0ccb3e53b4125213182776967a10b9bfa15a4fce
adib-commits <hdadib442@gmail.com>
2026-03-03 08:47:43 +0000

    the program compiles and runs

--- simple-calculator/calculator.c
+++ simple-calculator/calculator.c
@@ -5,18 +5,27 @@ int main(void)
     int choice;
 
     printf("Simple Calculator\n");
-    printf("1) Add\n");
-    printf("2) Subtract\n");
-    printf("3) Multiply\n");
-    printf("4) Divide\n");
-    printf("0) Quit\n");
 
-    printf("Choice: ");
-    scanf("%d", &choice);
-
-    if (choice == 0)
+    while (1)
     {
-        printf("Bye!\n");
+        printf("1) Add\n");
+        printf("2) Subtract\n");
+        printf("3) Multiply\n");
+        printf("4) Divide\n");
+        printf("0) Quit\n");
+
+        printf("Choice: ");
+        scanf("%d", &choice);
+
+        if (choice == 0)
+        {
+            printf("Bye!\n");
+            break;
+        }
+        else if (choice < 0 || choice > 4)
+        {
+            printf("Invalid choice\n");
+        }
     }
 
     return 0;
