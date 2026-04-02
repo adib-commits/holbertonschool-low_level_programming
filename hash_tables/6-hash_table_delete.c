199dfb007eb0ebbce6a39491be994addfd5714a9
adib-commits <hdadib442@gmail.com>
2026-04-02 13:56:32 +0000

    that deletes a hash table

--- hash_tables/6-hash_table_delete.c
+++ hash_tables/6-hash_table_delete.c
@@ -1,14 +1,15 @@
-#include "hash_tables.h"
 #include <stdlib.h>
+#include "hash_tables.h"
 
 /**
- * hash_table_delete - Deletes a hash table
+ * hash_table_delete - Deletes a hash table and frees all memory
  * @ht: The hash table to delete
  */
 void hash_table_delete(hash_table_t *ht)
 {
+	hash_node_t *node;
+	hash_node_t *next;
 	unsigned long int i;
-	hash_node_t *node, *tmp;
 
 	if (ht == NULL)
 		return;
@@ -18,13 +19,14 @@ void hash_table_delete(hash_table_t *ht)
 		node = ht->array[i];
 		while (node != NULL)
 		{
-			tmp = node->next;
+			next = node->next;
 			free(node->key);
 			free(node->value);
 			free(node);
-			node = tmp;
+			node = next;
 		}
 	}
+
 	free(ht->array);
 	free(ht);
 }
